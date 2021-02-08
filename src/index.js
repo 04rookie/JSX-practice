1b10f0bf2bc21c36a77721c0b3ebe87ba5c30b14
04rookie <47923475+04rookie@users.noreply.github.com>
2021-02-08 13:13:58 +0530

    using js functions

--- src/index.js
+++ src/index.js
@@ -6,3 +6,14 @@
 //E.g.
 //Created by Angela Yu.
 //Copyright 2019.
+import React from "react";
+import ReactDOM from "react-dom";
+var yearValue = new Date().getFullYear();
+
+ReactDOM.render(
+  <div>
+    <p>created by Atharva Jadhav</p>
+    <p>Copyright {yearValue}</p>
+  </div>,
+  document.getElementById("root")
+);
